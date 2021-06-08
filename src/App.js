f2e60cb46a8684835cc1c4a0436b12af65344730
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-08 14:23:06 +0300

    Finished Products List task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
-import campus from "./KhaldiyaCampus.jpg"
+import campus from "./KhaldiyaCampus.jpg";
+import products from "./products";
 
 function App() {
+
+  const booksList = products.map((book) => (
+    <div className="book">
+          <img className="booksImage" src={book.image} />
+          <p className="centerText">{book.name}</p>
+          <p className="centerText">{book.price} KD</p>
+    </div>
+  ));
+
   return (
     <div>
       <div className="centerText">
@@ -10,9 +20,11 @@ function App() {
         <p>My name is Sayed Mahmood Almohri. I am the founder and manager of this bookstore.</p>
         <p>Soon, we will have a wide range of books in our website store.</p>
         <p>Stay tuned!</p>
-        <img src={campus} />
+        <img className="storeImage" src={campus} />
       </div>
       
+      <div className="list">{booksList}</div>
+
     </div>
   );
 }
